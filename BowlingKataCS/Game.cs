4c796e80ee7edc5d70f2df9e485728d6cd3e84a1
skatworks <jean_paul_gaultier@msn.com>
2020-10-24 13:19:57 +0900

    initial commit.

---
+++ BowlingKataCS/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKataCS
+{
+    public class Game
+    {
+        private int sc = 0;
+
+        public void roll(int pins)
+        {
+            sc += pins;
+        }
+
+        public int score()
+        {
+            return sc;
+        }
+    }
+}
---
+++ BowlingKataCSTests/GameTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingKataCS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKataCS.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        private Game game;
+
+        [TestInitialize()]
+        public void init()
+        {
+            game = new Game();
+        }
+
+        // すべてガターの場合
+        [TestMethod()]
+        public void allGutterTest()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(0);
+            }
+            Assert.AreEqual(game.score(), 0);
+        }
+
+        // すべて1ピンだった場合
+        [TestMethod()]
+        public void allOneRollTest()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(1);
+            }
+            Assert.AreEqual(game.score(), 20);
+        }
+    }
+}
