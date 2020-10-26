597be0f1894a24830398816ce3bbc7da7d77a362
s.kogawa <s.kogawa@sbfw.co.jp>
2020-10-26 17:01:19 +0900

    ガターの場合、すべて1ピンの場合に対応。

---
+++ BowlingKataCS/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKataCS
+{
+    public class Game
+    {
+        private int score = 0;
+        public void roll(int pins)
+        {
+            score += pins;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+    }
+}
---
+++ BowlingKataCSTests/GameTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingKataCS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKataCS.Tests
+{
+    [TestClass()]
+    public class GameTest
+    {
+        private Game game;
+
+        [TestInitialize()]
+        public void init()
+        {
+            game = new Game();
+        }
+
+        [TestMethod()]
+        public void すべてガターの場合()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(0);
+            }
+            Assert.AreEqual(0, game.getScore());
+        }
+
+        [TestMethod()]
+        public void すべて1ピンの場合()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(1);
+            }
+            Assert.AreEqual(20, game.getScore());
+        }
+    }
+}
